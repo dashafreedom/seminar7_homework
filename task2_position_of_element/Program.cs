fc3ea9f60c3740e4411b8151cc0c08d0818e61e6
DaryaLukyanova <dasha.opt@mail.ru>
2022-12-05 19:54:23 +0500

    task2

---
+++ task2_position_of_element/Program.cs
@@ -0,0 +1,48 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+int rows = ReadInt("Введите количество строк: ");
+int columns = ReadInt("Введите количество столбцов: ");
+int[,] numbers = new int[rows, columns];
+FillMatrixRandomNumbers(numbers);
+WriteMatrix(numbers);
+
+int indRow = ReadInt("Введите индекс строки: ");
+int indColumn = ReadInt("Введите индекс столбца: ");
+
+
+if(indRow <= numbers.GetLength(0) && indColumn <= numbers.GetLength(1))
+    Console.WriteLine(numbers[indRow, indColumn]);  
+               
+else Console.WriteLine("Такого элемента не существует!");
+                
+            
+
+void FillMatrixRandomNumbers(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void WriteMatrix(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+    Console.WriteLine();
+    }
+Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
